8d1171bc00298e33a9885906c3b097d1a993d648
jinjinliang <3122702689@qq.com>
2022-10-10 17:01:22 +0800

    register-finished

---
+++ src/utils/vant.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import { Button, NavBar } from 'vant';
+import { Form } from 'vant';
+import { Field } from 'vant';
+
+Vue.use(Form);
+Vue.use(Field);
+Vue.use(NavBar);
+Vue.use(Button)
--- src/views/register.vue
+++ src/views/register.vue
@@ -1,15 +1,41 @@
 <template>
     <div>
-        注册页面
+        <van-nav-bar title="用户注册" />
+        <van-form @submit="onSubmit">
+            <van-field v-model="username" name="用户名" label="用户名" placeholder="用户名"
+                :rules="[{ required: true, message: '请填写用户名' }]" />
+            <van-field v-model="password" type="password" name="密码" label="密码" placeholder="密码"
+                :rules="[{ required: true, message: '请填写密码' }]" />
+            <div style="margin: 16px;">
+                <van-button block type="primary" native-type="submit">提交</van-button>
+            </div>
+        </van-form>
+        <div class="text-right">
+            <router-link to="/login">有账号，去登陆</router-link>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            username: '',
+            password: ''
 
+        }
+    },
+    methods: {
+        onSubmit(values) {
+            console.log('submit', values);
+        },
+    },
 }
 </script>
 
-<style>
-
+<style scoped>
+.text-right {
+    margin-right: 16px;
+    text-align: right;
+}
 </style>

af08d8ede68c8051275bb49cb1df4ee80f050288
jinjinliang <3122702689@qq.com>
2022-10-13 19:53:19 +0800

    likeCount-Collection

--- src/api/article.js
+++ src/api/article.js
@@ -21,3 +21,11 @@ export function getArticleDetail(id) {
 export function likeOrCollect(data) {
     return request.post('/interview/opt', data)
 }
+
+// 封装获取点赞/收藏列表接口
+// config -> { optType: 1点赞2收藏, page: 当前第几页 }
+export function getLikeOrCollectList(config) {
+    return request.get("/interview/opt/list", {
+        params: config
+    })
+}
--- src/views/favorite.vue
+++ src/views/favorite.vue
@@ -1,13 +1,48 @@
 <template>
-    <div>favorite</div>
+    <div>
+        <van-nav-bar title="我的点赞" fixed placeholder />
+        <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="loadMore">
+            <!-- 面经列表 -->
+            <article-item v-for="item in articleList" :key="item.id" :article="item"></article-item>
+        </van-list>
+    </div>
 </template>
-
+  
 <script>
+// 导入封装的获取点赞/收藏列表接口
+import { getLikeOrCollectList } from "../api/article";
 export default {
+    data() {
+        return {
+            loading: true,
+            finished: false,
+            articleList: [],
+            current: 1,
+        };
+    },
+    created() {
+        this.loadList();
+    },
+    methods: {
+        async loadList() {
+            const res = await getLikeOrCollectList({
+                optType: 1,
+                page: this.current,
+            });
+            console.log("点赞列表", res);
+            this.articleList = res.data.data.rows;
 
-}
+            // 关闭加载
+            this.loading = false;
+
+            // 判断是否有更多数据
+            this.finished = this.current >= res.data.data.pageTotal;
+        },
+        loadMore() { },
+    },
+};
 </script>
-
+  
 <style>
 
 </style>

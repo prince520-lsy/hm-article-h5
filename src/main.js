56ebbddda63a861e9d8c4f740eb4cc209adc5ab3
jinjinliang <3122702689@qq.com>
2022-10-20 10:15:01 +0800

    function-finished

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
 import './utils/vant'
-
 import ArticleItem from './components/article-item.vue'
+import router from './router'
+
 Vue.component("article-item", ArticleItem)
 Vue.config.productionTip = false
 
